c08687f94b6474de031c5bc104ce802e704a5c24
eugene liyai <eugeneliyai@ip-10-10-156-101.ec2.internal>
2019-04-12 15:15:48 +0300

    [feat: #] add canvas layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 window.onload = function () {
-
+    let canvas = document.getElementById('gameCanvas');
+    let canvasContext = canvas.getContext('2d');
+    canvasContext.fillStyle = 'blue';
+    canvasContext.fillRect(0,0, canvas.width, canvas.height);
 }
