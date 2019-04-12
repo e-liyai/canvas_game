c312ffac7acad95caf258f0857942797ae6ca008
eugene liyai <eugeneliyai@ip-10-10-156-101.ec2.internal>
2019-04-12 16:42:35 +0300

    [feat: #] separate moving and draw fill

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
+let canvas;
+let canvasContext;
+let ballx = 50;
+
 window.onload = function () {
-    let canvas = document.getElementById('gameCanvas');
-    let canvasContext = canvas.getContext('2d');
+    canvas = document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+
+    let framesPerSecond = 30;
+    setInterval(function() {
+        moveEverything();
+        drawEverything();
+    }, 1000/framesPerSecond);
+}
+
+function moveEverything() {
+    ballx = ballx + 5;
+}
+
+function drawEverything() {
+
     canvasContext.fillStyle = 'blue';
     canvasContext.fillRect(0,0, canvas.width, canvas.height);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(0,210, 10, 100);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballx,100, 10, 10);
 }
